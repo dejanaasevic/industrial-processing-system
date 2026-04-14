f376ba21b777f98102486a288b58e632aaebb80e
dejana šević <dejana.sevic24@gmail.com>
2026-04-14 19:22:30 +0200

    feature: add configuration loader #3

--- IndustrialProcessingSystem/Config/SystemConfig.cs
+++ IndustrialProcessingSystem/Config/SystemConfig.cs
@@ -3,4 +3,11 @@
     public int WorkerCount { get; set; }
     public int MaxQueueSize { get; set; }
     public List<Job> Jobs { get; set; } = new List<Job>();
+
+    public SystemConfig(int workerCount, int maxQueueSize, List<Job> jobs)
+    {
+        WorkerCount = workerCount;
+        MaxQueueSize = maxQueueSize;
+        Jobs = jobs;
+    }
 }
