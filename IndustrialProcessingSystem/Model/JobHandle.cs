1f72bd47be2505e6a9fb8e1b5889134b43b52ac5
dejana šević <dejana.sevic24@gmail.com>
2026-04-15 12:50:26 +0200

    add processing system logic #5

--- IndustrialProcessingSystem/Model/JobHandle.cs
+++ IndustrialProcessingSystem/Model/JobHandle.cs
@@ -2,4 +2,10 @@
 {
     public Guid Id { get; set; }
     public Task<int> Result { get; set; }
+
+    public JobHandle(Guid id, Task<int> result)
+    {
+        Id = id;
+        Result = result;
+    }
 }
