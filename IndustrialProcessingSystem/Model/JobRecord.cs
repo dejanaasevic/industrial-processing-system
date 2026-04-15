7b568637419bfda3a05fc811784257bfcd004cef
dejana šević <dejana.sevic24@gmail.com>
2026-04-15 20:22:23 +0200

    feature:add generate report logic #12

---
+++ IndustrialProcessingSystem/Model/JobRecord.cs
@@ -0,0 +1,14 @@
+﻿public class JobRecord
+{
+    public JobType Type { get; set; }
+    public bool Success { get; set; }
+    public TimeSpan Duration { get; set; }
+
+    public JobRecord(JobType type, bool success, TimeSpan duration)
+    {
+        Type = type;
+        Success = success;
+        Duration = duration;
+    }
+}
+
