f96652b4805e228bc94aae92452b08a812b91c5f
dejana šević <dejana.sevic24@gmail.com>
2026-04-15 22:12:17 +0200

    feat: implement main entry point and wire up processing, logging, and reporting components #13

--- IndustrialProcessingSystem/Program.cs
+++ IndustrialProcessingSystem/Program.cs
@@ -25,5 +25,49 @@ class Program
 
         processingSystem.JobAborted += (sender, e) =>
             reportGenerator.RecordJob(e.Type, false, e.Duration);
+
+        int workerCount = config.WorkerCount;
+        List<Task> producerTasks = new List<Task>();
+        for(int i = 0; i < workerCount; i++)
+        {
+            producerTasks.Add(Task.Run( async () =>
+            {
+                Random random = Random.Shared;
+                while (true)
+                {
+                    try
+                    {
+                        Job job = GenerateRandomJob(random);
+                        processingSystem.Submit(job);
+                    }
+                    catch(Exception ex){
+                        Console.WriteLine($"Submit failed: {ex.Message}");
+                    }
+                    await Task.Delay(random.Next(5000, 10000));
+                }
+
+            }));
+        }
+        await Task.WhenAll(producerTasks);
+    }
+
+    private static Job GenerateRandomJob(Random random)
+    {
+        JobType type = (JobType)random.Next(0, 2);
+        string payload = String.Empty;
+        if(type == JobType.Prime)
+        {
+            int numbers = random.Next(1, 10);
+            int threads = random.Next(1, 10);
+            payload = $"numbers:{numbers}_000,threads:{threads}";
+        }
+        else
+        {
+            int delay = random.Next(1, 20);
+            payload = $"delay:{delay}_000";
+        }
+
+        int priority = random.Next(1, 6);
+        return new Job(type, payload, priority);
     }
 }
